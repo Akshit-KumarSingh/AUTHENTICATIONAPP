deb64c27d3e31b1fc3a11bbc2c067abdbac6875d
Akshit-Kumar <akshit.krsingh.mec23@itbhu.ac.in>
2025-07-15 22:57:04 +0530

    Initial backend commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import { connectDB } from "./db/connectDB.js";
+dotenv.config();
+import authRoutes from "./routes/auth.route.js";
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server is running on port ", PORT);
+});
